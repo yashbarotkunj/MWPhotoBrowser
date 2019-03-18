eb7864285324462a6d477c9d71cc122bceae9a89
Yash Barot <yashbarot.kunj@gmail.com>
2019-03-18 12:46:39 +0530

    commit cdoe

--- Pod/Classes/MWPhoto.m
+++ Pod/Classes/MWPhoto.m
@@ -6,7 +6,7 @@
 //  Copyright 2010 d3i. All rights reserved.
 //
 
-#import <SDWebImage/SDWebImageDecoder.h>
+//#import <SDWebImage/SDWebImageDecoder.h>
 #import <SDWebImage/SDWebImageManager.h>
 #import <SDWebImage/SDWebImageOperation.h>
 #import <AssetsLibrary/AssetsLibrary.h>
